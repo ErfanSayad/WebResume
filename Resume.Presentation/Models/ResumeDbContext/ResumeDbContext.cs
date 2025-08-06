aa341d642788c3350b98d6bc48c1ee7b9f9ba8b2
ESPC <erfan.sayad@yahoo.com>
2025-08-06 11:58:08 +0330

    Add Education Controller

---
+++ Resume.Presentation/Models/ResumeDbContext/ResumeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resume.Presentation.Models.Entities.Education;
+using Resume.Presentation.Models.Entities.Experience;
+using Resume.Presentation.Models.Entities.MySkills;
+namespace Resume.Presentation.Models.ResumeDbContext;
+
+public class ResumeDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=ERFANPC;Initial Catalog=TestDbContext;Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<Experience> experiences { get; set; }
+    public DbSet<Education> educations { get; set; }
+    public DbSet<MySkills> mySkills { get; set; }
+}
